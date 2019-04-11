7e22931095564ee89a34319eaecd524841350c4f
roche <lonagcn@163.com>
2019-04-11 21:04:38 +0800

    中文

--- futaba.php
+++ futaba.php
@@ -80,14 +80,14 @@ function form(&$dat,$resno,$admin=""){
   $maxbyte = MAX_KB * 1024;
   $no=$resno;
   if($resno){
-    $msg .= "[<a href=\"".PHP_SELF2."\">掲示板に戻る</a>]\n";
+    $msg .= "[<a href=\"".PHP_SELF2."\">回到公告牌</a>]\n";
     $msg .= "<table width='100%'><tr><th bgcolor=#e04000>\n";
-    $msg .= "<font color=#FFFFFF>レス送信モード</font>\n";
+    $msg .= "<font color=#FFFFFF>无发送模式</font>\n";
     $msg .= "</th></tr></table>\n";
   }
   if($admin){
     $hidden = "<input type=hidden name=admin value=\"".ADMIN_PASS."\">";
-    $msg = "<h4>タグがつかえます</h4>";
+    $msg = "<h4>标签堵塞</h4>";
   }
   $dat.=$msg.'<center>
 <form action="'.PHP_SELF.'" method="POST" enctype="multipart/form-data">
@@ -98,23 +98,23 @@ function form(&$dat,$resno,$admin=""){
 if($no){$dat.='<input type=hidden name=resto value="'.$no.'">
 ';}
 $dat.='<table cellpadding=1 cellspacing=1>
-<tr><td bgcolor=#eeaa88><b>おなまえ</b></td><td><input type=text name=name size="28"></td></tr>
+<tr><td bgcolor=#eeaa88><b>你的名字</b></td><td><input type=text name=name size="28"></td></tr>
 <tr><td bgcolor=#eeaa88><b>E-mail</b></td><td><input type=text name=email size="28"></td></tr>
-<tr><td bgcolor=#eeaa88><b>題　　名</b></td><td><input type=text name=sub size="35">
-<input type=submit value="送信する"></td></tr>
-<tr><td bgcolor=#eeaa88><b>コメント</b></td><td><textarea name=com cols="48" rows="4" wrap=soft></textarea></td></tr>
+<tr><td bgcolor=#eeaa88><b>题目名</b></td><td><input type=text name=sub size="35">
+<input type=submit value="发送"></td></tr>
+<tr><td bgcolor=#eeaa88><b>注释</b></td><td><textarea name=com cols="48" rows="4" wrap=soft></textarea></td></tr>
 ';
 if(RESIMG || !$resno){
-$dat.='<tr><td bgcolor=#eeaa88><b>添付File</b></td>
+$dat.='<tr><td bgcolor=#eeaa88><b>附件File</b></td>
 <td><input type=file name=upfile size="35">
-[<label><input type=checkbox name=textonly value=on>画像なし</label>]</td></tr>
+[<label><input type=checkbox name=textonly value=on>无图像</label>]</td></tr>
 ';}
-$dat.='<tr><td bgcolor=#eeaa88><b>削除キー</b></td><td><input type=password name=pwd size=8 maxlength=8 value=""><small>(記事の削除用。英数字で8文字以内)</small></td></tr>
+$dat.='<tr><td bgcolor=#eeaa88><b>删除键</b></td><td><input type=password name=pwd size=8 maxlength=8 value=""><small>(記事の削除用。英数字で8文字以内)</small></td></tr>
 <tr><td colspan=2>
 <small>
 <LI>添付可能ファイル：GIF, JPG, PNG ブラウザによっては正常に添付できないことがあります。
-<LI>最大投稿データ量は '.MAX_KB.' KB までです。sage機能付き。
-<LI>画像は横 '.MAX_W.'ピクセル、縦 '.MAX_H.'ピクセルを超えると縮小表示されます。
+<LI>最大投稿ー量 '.MAX_KB.' KB までです。sage機能付き。
+<LI>图像为横向 '.MAX_W.'像素，垂直 '.MAX_H.'图像为横向
 '.$addinfo.'</small></td></tr></table></form></center><hr>';
 }
 /* 記事部分 */
@@ -181,7 +181,7 @@ function updatelog($resno=0){
     // メイン作成
     $dat.="<input type=checkbox name=\"$no\" value=delete><font color=#cc1105 size=+1><b>$sub</b></font> \n";
     $dat.="Name <font color=#117743><b>$name</b></font> $now No.$no &nbsp; \n";
-    if(!$resno) $dat.="[<a href=".PHP_SELF."?res=$no>返信</a>]";
+    if(!$resno) $dat.="[<a href=".PHP_SELF."?res=$no>回复</a>]";
     $dat.="\n<blockquote>$com</blockquote>";
      // そろそろ消える。
      if($lineindex[$no]-1 >= LOG_MAX*0.95){
@@ -240,7 +240,7 @@ function updatelog($resno=0){
     if($resno){break;} //res時はtree1行だけ
   }
 $dat.='<table align=right><tr><td nowrap align=center>
-<input type=hidden name=mode value=usrdel>【記事削除】[<input type=checkbox name=onlyimgdel value=on>画像だけ消す]<br>
+<input type=hidden name=mode value=usrdel>【記事削除】[<input type=checkbox name=onlyimgdel value=on>只删除图像]<br>
 削除キー<input type=password name=pwd size=8 maxlength=8 value="">
 <input type=submit value="削除"></form></td></tr></table>';
     if(!$resno){ //res時は表示しない
@@ -268,7 +268,7 @@ $dat.='<table align=right><tr><td nowrap align=center>
       $dat.="</td>";
       if($p >= PAGE_DEF && count($tree) > $next){
         $dat.="<form action=\"".$next/PAGE_DEF.PHP_EXT."\" method=get><td>";
-        $dat.="<input type=submit value=\"次のページ\">";
+        $dat.="<input type=submit value=\"下一页\">";
         $dat.="</td></form>";
       }else{$dat.="<td>最后一页</td>";}
         $dat.="</tr></table><br clear=all>\n";
